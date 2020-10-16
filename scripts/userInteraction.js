20de44b0de22107273ec106fe5bdaffa43d658e3
Leon <leonlit123@gmail.com>
2020-10-16 21:52:02 +0800

    Code refactoring and some fixed
    Added a new shader for about page. Also, changed the way user close the about page and error message pop-up by clicking the shader.

--- scripts/userInteraction.js
+++ scripts/userInteraction.js
@@ -53,32 +53,43 @@ function isEnterMain (event) {
 	}
 }
 
+const page = document.getElementById("aboutPage");
+const shader3 = document.getElementById("shader3");
 //showing and closing the about page for the app
 function openClosePage () {
 	//to close the search list
 	insertValue("");
-	const page = document.getElementById("aboutPage");
 	if (!pageDisabled) {
 		if (pageOpen) {
-			pageOpen = false;
-			page.classList.remove("fade-in-left-03");
-			page.classList.add("fade-out-03");
-			
-			//locking the page so that the page animation could be finished first before user could interact
-			//with the about page close button
-			lockPage(300);
-			setTimeout(() => {
-				page.style.display = "none";
-			}, 300);
+			closePage();
 		}else {
-			if (navOpen) openCloseNav();
-			pageOpen = true;
-			lockPage(300);
-			page.style.display = "block";
-			page.classList.remove("fade-out-03");
-			page.classList.add("fade-in-left-03");
+			openPage();
 		}
 	}
+}
+
+function closePage () {
+	pageOpen = false;
+	page.classList.remove("fade-in-left-03");
+	page.classList.add("fade-out-03");
+	makeShaderDisappear(shader3);
+	
+	//locking the page so that the page animation could be finished first before user could interact
+	//with the about page close button
+	lockPage(300);
+	setTimeout(() => {
+		page.style.display = "none";
+	}, 300);
+}
+
+function openPage () {
+	if (navOpen) openCloseNav();
+	makeShaderAppear(shader3);
+	pageOpen = true;
+	lockPage(300);
+	page.style.display = "block";
+	page.classList.remove("fade-out-03");
+	page.classList.add("fade-in-left-03");
 }
 
 //used to lock the navigation bar so that the animation won't be chuncky 
@@ -144,17 +155,13 @@ function closeNav () {
 	navBar[1].className = "appear";
 	navBar[0].className = "rotateBackNav45";
 	navBar[2].className = "rotateBackNav-45";
-	shader.classList.remove("appear");
-	shader.classList.add("hidden")
 	navItems.classList.add("fade-out-up");
 	navItems.classList.remove("fade-in-down");
 	navOpen = false;
 
 	adjustNavCont();
-
 	lockNav(400);
-
-	shader.style.opacity = "0";
+	makeShaderDisappear(shader);
 	navTitle.style.color = "black";
 
 	setTimeout(()=>{
@@ -162,9 +169,7 @@ function closeNav () {
 		navBar[2].style.transform = "translate(0, 0) rotate(0deg)";
 		navBar[0].style.backgroundColor = "black";
 		navBar[2].style.backgroundColor = "black";
-		navBar[1].style.opacity = 1;
-		shader.style.display = "none";
-		
+		navBar[1].style.opacity = 1;		
 	}, 400);
 	
 	navItems.style.top = "-260px";
@@ -179,29 +184,25 @@ function openNav () {
 	navItems.classList.add("fade-in-down");
 	navItems.classList.remove("fade-out-up");
 	navOpen = true;
-	shader.style.opacity = "0";
-	shader.style.display = "block";
-	shader.classList.remove("hidden");
-	shader.classList.add("appear");
-
+	
 	lockNav(400);
+	makeShaderAppear(shader)
 
 	setTimeout(()=> {
 		navBar[1].style.opacity = 0;
 	},200)
-
-	shader.style.opacity = "1";
+	
 	navTitle.style.color = "white";
 	navShadow.style.boxShadow = `0px 1px 10px rgba(128, 128, 128 , 1)`;
+	navItems.style.top = "0px";
 
 	setTimeout(()=>{
 		navBar[0].style.transform = "translate(0, 10px) rotate(45deg)";
 		navBar[2].style.transform = "translate(0, -5px) rotate(-45deg)";
 		navBar[0].style.backgroundColor = "white";
 		navBar[2].style.backgroundColor = "white";
-		
 	}, 400);
-	navItems.style.top = "0px";
+	
 }
 
 const container = document.getElementById("errorPopUp");
@@ -214,7 +215,7 @@ function openCloseError (message) {
     if (!errorShown) {
         openError(message);
     }else {
-        closeError(message);
+        closeError();
     }
 }
 
@@ -228,30 +229,41 @@ function openError (message) {
 	container.classList.remove("hidden");
 	container.classList.add("appear");
 
-	shader2.style.opacity = "0";
-	shader2.style.display = "block";
-	shader2.classList.remove("hidden");
-	shader2.classList.add("appear");
-
+	makeShaderAppear(shader2);
 	setTimeout(() => {
-		shader2.style.opacity = "1";
 		container.style.opacity = "1";
 	}, 400);
 }
 
 //close the error window
-function closeError (message) {
+function closeError () {
 	errorShown = false;     
 	container.classList.remove("appear");
 	container.classList.add("hidden");
 
-	shader2.classList.remove("appear");
-	shader2.classList.add("hidden");
-	
+	makeShaderDisappear(shader2);
+
 	setTimeout(()=>{
-		shader2.style.display = "none";
-		shader2.style.opacity = "0";
 		container.style.display = "none";
 		container.style.opacity = "0";
+	}, 400);
+}
+
+
+function makeShaderAppear (shader) {
+	shader.style.opacity = "0";
+	shader.style.display = "block";
+	shader.classList.remove("hidden");
+	shader.classList.add("appear");
+	setTimeout(() => {
+		shader.style.opacity = "1";
+	}, 200);
+}
+
+function makeShaderDisappear (shader) {
+	shader.classList.remove("appear");
+	shader.classList.add("hidden");
+	setTimeout(() => {
+		shader.style.display = "none";
 	}, 400);
 }
