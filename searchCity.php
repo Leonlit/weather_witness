e073d05f96a07841dbfc282700877a54cdc5e9f0
Leon <leonlit123@gmail.com>
2021-03-07 22:35:54 +0800

    added some securiy and error handling

--- searchCity.php
+++ searchCity.php
@@ -1,21 +1,33 @@
 <?php
-    $param = $_GET["city"];
-    if (strlen($param) > 2){
-        $file = fopen("cityObject.txt","r");
-
-        while(! feof($file)) {
-            $content = fgets($file);
-            $line = explode(",", $content);//.split(",");
+    try{
+        if (!isset($_GET["city"])){
+            throw new Exception();
         }
-        fclose($file);
+        $param = filter_var($_GET["city"], FILTER_SANITIZE_STRING);
+        if (strlen($param) > 2){
+            try {                
+                $file = fopen("cityObject.txt","r");
 
-        $found = [];
-        foreach ($line as $city) {
-            if (stripos($city, $param)) {
-                array_push($found, $city);
+                while(! feof($file)) {
+                    $content = fgets($file);
+                    $line = explode(",", $content);
+                }
+                fclose($file);
+            } catch (Exception $ex) {
+                http_response_code(500);
             }
+            $found = [];
+            foreach ($line as $city) {
+                if (stripos($city, $param)) {
+                    array_push($found, $city);
+                }
+            }
+            header("Content-Type: application/json; charset=UTF-8");
+            echo json_encode($found);
+        }else {
+            throw new Exception();
         }
-        header("Content-Type: application/json; charset=UTF-8");
-        echo json_encode($found);
+    }catch(Exception $ex){
+        http_response_code(422);
     }
 ?>
