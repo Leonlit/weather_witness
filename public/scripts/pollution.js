ecb913fe9caf33622a4927e121d74e248cc3e37a
Leon <leonlit123@gmail.com>
2021-05-22 10:25:45 +0800

    fixed css and js code

--- public/scripts/pollution.js
+++ public/scripts/pollution.js
@@ -13,17 +13,18 @@ async function getPollutionData (coord) {
 
 function showPollutionData () {
     console.log(pollutionData);
+    const unit = " μg/m<sup>3</sup>";
     const data = pollutionData["list"][0];
     const pollutionContainer = document.getElementById("pollutionDataContainer");
     const pollutionFields = pollutionContainer.getElementsByClassName("pollutionData");
-    pollutionFields[0].innerHTML = limitSignificantShown(data["main"]["aqi"], 2);
-    pollutionFields[1].innerHTML = limitSignificantShown(data["components"]["co"], 2);
-    pollutionFields[2].innerHTML = limitSignificantShown(data["components"]["no"], 2);
-    pollutionFields[3].innerHTML = limitSignificantShown(data["components"]["no2"], 2);
-    pollutionFields[4].innerHTML = limitSignificantShown(data["components"]["o3"], 2);
-    pollutionFields[5].innerHTML = limitSignificantShown(data["components"]["so2"], 2);
-    pollutionFields[6].innerHTML = limitSignificantShown(data["components"]["nh3"], 2);
-    pollutionFields[7].innerHTML = limitSignificantShown(data["components"]["pm2_5"], 2);
-    pollutionFields[8].innerHTML = limitSignificantShown(data["components"]["pm10"], 2);
+    pollutionFields[0].innerHTML = limitSignificantShown(data["main"]["aqi"], 2) + unit;
+    pollutionFields[1].innerHTML = limitSignificantShown(data["components"]["co"], 2) + unit;
+    pollutionFields[2].innerHTML = limitSignificantShown(data["components"]["no"], 2) + unit;
+    pollutionFields[3].innerHTML = limitSignificantShown(data["components"]["no2"], 2) + unit;
+    pollutionFields[4].innerHTML = limitSignificantShown(data["components"]["o3"], 2) + unit;
+    pollutionFields[5].innerHTML = limitSignificantShown(data["components"]["so2"], 2) + unit;
+    pollutionFields[6].innerHTML = limitSignificantShown(data["components"]["nh3"], 2) + unit;
+    pollutionFields[7].innerHTML = limitSignificantShown(data["components"]["pm2_5"], 2) + unit;
+    pollutionFields[8].innerHTML = limitSignificantShown(data["components"]["pm10"], 2) + unit;
 }
 
